8fc8f1d665d27927cf3a84d617bd2e82a918de28
Sebastian Cloos Hylander <sehy@itu.dk>
2026-02-23 10:43:42 +0100

    session 04

---
+++ session_04/FunWithBind/Program.fs
@@ -0,0 +1,29 @@
+﻿
+let rng = System.Random()
+let malicious_fun i = if rng.Next 100 > 10 then Some i else None
+
+
+let add5 a = a + 5
+let mul3_fault a = malicious_fun (a * 3)
+
+let add5_on_opt =
+    let x_opt = malicious_fun 10    
+    match x_opt with
+    | Some x -> Some(add5 x)
+    | None -> None
+
+let (>>=) opt binder = Option.bind binder opt 
+
+
+let add5_on_opt_2 () =
+    let x_opt = malicious_fun 10  
+
+    x_opt 
+    |> Option.bind ( 
+        fun x -> mul3_fault x 
+                 |> Option.bind (fun y -> Some (add5 y)))
+
+let add5_on_opt_3 () =
+    let x_opt = malicious_fun 10  
+
+    x_opt >>= fun x -> mul3_fault x >>= fun y -> Some (add5 y)
---
+++ session_04/message/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let sc = Kattio.Scanner()
+
+let rows = sc.NextInt()
+let cols = sc.NextInt()
+
+let secret_message: char list = List.Empty
+
+
+let wrapper =
+    List.init rows (fun _ -> sc.Next())
+    |> List.map (fun item -> Array.filter (fun ch -> ch <> '.') (item.ToCharArray()) |>  System.String.Concat) |> System.String.Concat
+
+printf "%s" wrapper
